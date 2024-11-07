bb6721b72858de8f378f2334f12492ca016ec5b1
Jon Clegg <jon.clegg@paramount.com>
2024-11-07 17:46:01 -0600

    fixed location stuff

--- MeetTracker/LocationManager.swift
+++ MeetTracker/LocationManager.swift
@@ -24,4 +24,21 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
             manager.stopUpdatingLocation()
         }
     }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
+    }
+
+    var locationStatus: String {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            return "Please allow location access"
+        case .restricted, .denied:
+            return "Location access denied"
+        case .authorizedWhenInUse, .authorizedAlways:
+            return ""
+        @unknown default:
+            return "Unknown location status"
+        }
+    }
 }
